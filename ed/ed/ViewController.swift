cee781e69d90dc3121496521df955de599259b4a
Михали <89198160+HouseForMouse@users.noreply.github.com>
2022-04-28 12:08:55 +0300

    Add files via upload
    free mark ez way to die on low life
    \

---
+++ ed/ed/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ed
+//
+//  Created by Student on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBOutlet weak var inputLogin: UITextField!
+    @IBOutlet weak var inputPassword: UITextField!
+    
+   
+}
+
